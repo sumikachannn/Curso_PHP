415e964f20ed8980b669b988ed7266b187117d63
sumikachannn <vkenzoyamane@gmail.com>
2021-11-12 21:30:11 -0300

    Update do sobre.php
    Coisa basica pra aparecer a caixa de "marcado" assim que a gente for melhorando, na próxima vai ser uma boa colocar uma permissão especifica de edição dependendo do login

--- concessionaria_v2/sobre.php
+++ concessionaria_v2/sobre.php
@@ -30,15 +30,20 @@
 			<li>Giovani Zeni;</li>
 			<li>Vinicius Kenzo Yamane;</li>
 			<hr>
-			<h2>Recursos do sistema:</h2>
-			<li>Página sobre;</li>
-			<li>Conexão com o banco de dados</li>
-			<li>Listar os usuários registrados;</li>
-			<li>Alterar a ordem de exibição dos usuários na listagem;</li>
-			<li>Editar um usuário registrado;</li>
-			<li>Adicionar um usuário novo, com permissões de acesso diferentes;</li>
-			<li>Excluir um usuário registrado;</li>
-			<li>Abrir sessões diferentes dependendo do nível de permissão do usuário;</li>
+			<h3>Recursos do sistema:</h3>
+			<input type="checkbox" disabled="disabled" checked="checked">Página sobre;<br>
+			<input type="checkbox" disabled="disabled" checked="checked">Conexão com o banco de dados<br>
+			<input type="checkbox" disabled="disabled" checked="checked">Listar os usuários registrados;<br>
+			<input type="checkbox" disabled="disabled" checked="checked">Alterar a ordem de exibição dos usuários na listagem;<br>
+			<input type="checkbox" disabled="disabled" checked="checked">Editar um usuário registrado;<br>
+			<input type="checkbox" disabled="disabled" checked="checked">Adicionar um usuário novo, com permissões de acesso diferentes;<br>
+			<input type="checkbox" disabled="disabled" checked="checked">Excluir um usuário registrado;<br>
+			<input type="checkbox" disabled="disabled" checked="checked">Abrir sessões diferentes dependendo do nível de permissão do usuário;<br>
+			<input type="checkbox" disabled="disabled">Página individual para cada cliente/carro<br>
+			<input type="checkbox" disabled="disabled">Link das imagens com uma página de cliente/carro<br>
+			<input type="checkbox" disabled="disabled">Página de veiculos em formato de galeria ou por scroll<br>
+			<input type="checkbox" disabled="disabled">Filtragem das listas por atributo ou pesquisa especifica<br>
+
 	</div>
 	</p>
 	<hr>
